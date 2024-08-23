644cfa4538e0159cc6fed6e8aa1a1f5adeb328cc
RealGabrielDelRosario <gabriel.p.delrosario@student.ateneo.edu>
2024-08-23 22:47:54 +0800

    Added a sample level in the main scene

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody3D
 
+@onready var _player_visual: Node3D  = $PlayerVisual
+
+var _physics_body_trans_last: Transform3D
+var _physics_body_trans_current: Transform3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -7,8 +11,13 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready() -> void:
+	#prevents the visual node from being affected by the movement of its parent
+	_player_visual.top_level = true
 
 func _physics_process(delta):
+	_physics_body_trans_last = _physics_body_trans_current
+	_physics_body_trans_current = global_transform
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -22,10 +31,20 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		#trying out interpolation
+		velocity.x = lerp(velocity.x, direction.x * SPEED, 0.1)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, 0.1)
+		#velocity.x = direction.x * SPEED 
+		#velocity.z = direction.z * SPEED 
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.z = move_toward(velocity.z, 0, SPEED * delta)
 
 	move_and_slide()
+	
+func _process(_delta: float) -> void:
+	_player_visual.global_transform = \
+		_physics_body_trans_last.interpolate_with(
+			_physics_body_trans_current,
+			Engine.get_physics_interpolation_fraction()
+		)
