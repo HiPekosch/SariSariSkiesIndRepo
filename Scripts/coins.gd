5309b7e1850e5934e9ddc94e78b4d7fe7a9e1bcf
HiPekosch <pekoschcodes@gmail.com>
2024-10-17 15:27:57 +0800

    Should be working now

--- Scripts/coins.gd
+++ Scripts/coins.gd
@@ -4,16 +4,15 @@ var coinsCollected: int = 0
 @onready var coins = $"../../CanvasLayer/Coins"
 @onready var race_finished = $"../../CanvasLayer/Race Finished"
 
+func _physics_process(delta):
+	rotation.y += 2.0 * delta
+
 func _on_body_entered(body):
 	if(body.is_in_group("Kite")):
 		add_points()
 		queue_free()
-		
-	
-func _physics_process(delta):
-	rotation.y += 2.0 * delta
 	
 func add_points():
+	coinsCollected = coinsCollected + 1
 	print( "Coins Collected Value: " + str(coinsCollected))
-	coinsCollected += 1
 	coins.text = "Coins Collected: " + str(coinsCollected)
